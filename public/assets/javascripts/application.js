d17420406818795463205e47aa8f5cd7315189e6
Arius <ariusleechihin@gmail.com>
2012-07-11 19:44:56 +0800

    Create slider.

---
+++ public/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#home_slider').slides({
+    pagination: true,
+    generatePagination: false,
+    paginationClass: 'home_slider_pagination',
+    play: 5000,
+    currentClass: 'current',
+    pause: 0,
+    hoverPause: false,
+    generateNextPrev: false
+  });
+});
